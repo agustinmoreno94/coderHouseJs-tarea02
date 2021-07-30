8973734c209402476b50c8c0919e7ea5084e2a04
agustinmoreno94 <agustin.moreno94@gmail.com>
2021-07-29 23:30:58 -0300

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+let personajeA = 98;
+let personajeB = 49;
+
+let tuPersonaje = prompt("ingrese un numero entre el uno al 100")
+
+if (personajeB <= tuPersonaje){
+    if (personajeA <= tuPersonaje){
+        alert("tu personaje es el mas poderoso")
+    }else{
+        alert("tu personaje es mas fuerte que el B pero no del A")
+    }
+}else{
+    alert("tu personaje es debil")
+}
